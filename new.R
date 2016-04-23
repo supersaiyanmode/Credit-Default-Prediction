fe9e16ab6fef63ef228b9127d9e4420cdceb6223
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-04-23 08:10:47 -0400

    Lots of bug fixes.

--- new.R
+++ new.R
@@ -1,7 +1,6 @@
 library(data.tree)
 
 data = read.csv("credit.csv",header=TRUE,skip=1)
-pairs(data[,12:17])
 
 entropy <- function(responses) {
   sum = 0
@@ -13,9 +12,9 @@ entropy <- function(responses) {
 }
 
 id3 <- function(node, features, responses) {
-  if (length(unique(responses)) == 1) {
+  if (nrow(unique(responses)) == 1) {
     node$splitBy = NULL
-    node$response = unique(responses)
+    node$response = responses[1,1]
     return()
   }
   
@@ -25,28 +24,30 @@ id3 <- function(node, features, responses) {
   for (feature_index in 1:ncol(features)) {
     sum_entropy = 0
     for (unique_val in unique(features[,feature_index])) {
-      sum_entropy = sum_entropy + entropy(features[features[,feature_index] == unique_val, c(-feature_index)])
+      subset = responses[features[,feature_index] == unique_val,]
+      sum_entropy = sum_entropy + length(subset) / nrow(features) * entropy(subset)
     }
-    if (min_feature_entropy < sum_entropy) {
+    if (min_feature_entropy > sum_entropy) {
       min_feature_entropy =  sum_entropy
       min_feature_entropy_index = feature_index
     }
   }
   
-  node$splitBy = min_feature_entropy_index
-  for (unique_val in unique(features)) {
-    child = node$AddChild(colnames(features)[min_feature_entropy_index])
+  node$splitBy = colnames(features)[min_feature_entropy_index]
+  for (unique_val in unique(features[,min_feature_entropy_index])) {
+    child = node$AddChild(as.character(paste(colnames(features)[min_feature_entropy_index],"=",unique_val)))
     id3(child,
         features[features[min_feature_entropy_index]==unique_val, -c(min_feature_entropy_index)], 
-        responses[features[min_feature_entropy_index]==unique_val])
+        responses[features[min_feature_entropy_index]==unique_val,,drop=FALSE])
   }
 }
 
 dtree <- function(features, responses) {
   node = Node$new("root")
-  id3(node, features, responses)
+  id3(node, features, as.vector(responses))
   return(node)
 }
 data("mushroom")
 
 res = dtree(mushroom[,1:3], mushroom[4])
+print(res, "splitBy", "response")
